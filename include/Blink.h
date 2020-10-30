59321a3292ca2484ef2645c77d879b5b4e39b6e0
Mike Long <mikelong1971@gmail.com>
2020-10-30 00:28:17 -0700

    Color sweep working

--- include/Blink.h
+++ include/Blink.h
@@ -1,10 +1,25 @@
 #include <Arduino.h>
 
 // global variables
-unsigned long timer0,timer1,dutyTime,cycleTime;
+
+// timers
+unsigned long timer0,timer1,loopTime0,loopTime1;
+int loopCounter;
+// white pulse code
+unsigned long dutyTime,cycleTime;
 unsigned long pwmTime,pwmDuty,pulseUpdateTime;
 unsigned long pulseCycleTime,pwmAdv;
 bool ledON,countUP;
 
+// RGB color changing code
+// cycle R->G->B->R moving PWM window through each
+unsigned long pwmR,pwmG,pwmB,colorAdv;
+unsigned long colorCycleTime,colorUpdateTime;
+bool ledOnR,ledOnG,ledOnB;
+int pinR = PIN_A3;
+int pinG = PIN_A4;
+int pinB = PIN_A5;
+
 // function definitions
 void pulseWhite();
+void sweepColor();
--- src/Blink.cpp
+++ src/Blink.cpp
@@ -10,6 +10,10 @@ void setup()
   // setup serial monotor
   Serial.begin(115200);
   Serial.println("Hello!");
+  loopCounter = 0;
+  loopTime0 = millis();
+  loopTime1 = loopTime0;
+
   // initialize LED digital pin as an output.
   pinMode(PIN_A0, OUTPUT);
   timer0 = micros();        // syncronize counters to start
@@ -23,22 +27,135 @@ void setup()
   pulseUpdateTime = 100;     // time between brightness updates (microseconds) 
   ledON = false;            // is LED on?
   countUP = true;           // brightness counter direction
+
+  // color changing LED
+  pwmR = pwmTime;
+  pwmG = 0;
+  pwmB = 0;
+  pinMode(pinR,OUTPUT);
+  pinMode(pinG,OUTPUT);
+  pinMode(pinB,OUTPUT);
+  colorCycleTime = 0;
+  colorUpdateTime = 200;
+  colorAdv = 10;
 }
 
 void loop()
 {
     cycleTime = timer1-timer0;  // get time since last loop started
+    dutyTime += cycleTime;      // update the duty cycle timer
+    pulseCycleTime += cycleTime;  // update the PWM cycle time
+    colorCycleTime += cycleTime;  // update color cycle time
     // update the white LED
     pulseWhite();
+    sweepColor();
     // increment the timer for the next loop
     timer0 = timer1;
     timer1 = micros();
+    // output loop count to serial monitor every second
+    loopTime1 = millis();
+    loopCounter += 1;
+    if(loopTime1-loopTime0 >= 1000)
+    {
+      // example of how to write debug info to the serial terminal
+      char outString[60];
+      sprintf(outString, "loop count = %d per second", loopCounter);
+      Serial.println(outString);
+      loopTime0 = loopTime1;
+      loopCounter = 0;
+    }
+}
+
+void sweepColor()
+{
+  // vary PWM strength between RGB pins to change color
+
+  // sweep the pwm between RGB
+  if(colorCycleTime > colorUpdateTime)
+  {
+    colorCycleTime = 0;
+    int advSlow = colorAdv/3;
+    // take from Red and add to Green
+    if(pwmR > 0 && pwmB == 0)
+    {
+      if(pwmR < pwmTime/2)
+      {
+        pwmR += -advSlow;
+        pwmG += advSlow;
+      }
+      else
+      {
+        pwmR += -colorAdv;
+        pwmG += colorAdv;
+      }
+    }
+    // take from Green and add to Blue
+    if(pwmG > 0 && pwmR == 0)
+    {
+      if(pwmG < pwmTime/3)
+      {
+        pwmG += -advSlow;
+        pwmB += advSlow;
+      }
+      else
+      {
+        pwmG += -colorAdv;
+        pwmB += colorAdv;
+      }
+    }
+    // take from Blue and add to Red
+    if(pwmB > 0 && pwmG == 0)
+    {
+      if(pwmB < pwmTime/2)
+      {
+        pwmB += -advSlow;
+        pwmR += advSlow;
+      }
+      else
+      {
+        pwmB += -colorAdv;
+        pwmR += colorAdv;
+      }
+    }
+  }
+
+  // set OFF state
+  if(dutyTime > pwmR && ledOnR)
+  {
+    digitalWrite(pinR,LOW);
+    ledOnR = false;
+  }
+  if(dutyTime > pwmG && ledOnG)
+  {
+    digitalWrite(pinG,LOW);
+    ledOnG = false;
+  }
+  if(dutyTime > pwmB && ledOnB)
+  {
+    digitalWrite(pinB,LOW);
+    ledOnB = false;
+  }
+  // set ON state
+  if(dutyTime < pwmR && !ledOnR)
+  {
+    digitalWrite(pinR,HIGH);
+    ledOnR = true;
+  }
+  if(dutyTime < pwmG && !ledOnG)
+  {
+    digitalWrite(pinG,HIGH);
+    ledOnG = true;
+  }
+  if(dutyTime < pwmB && !ledOnB)
+  {
+    digitalWrite(pinB,HIGH);
+    ledOnB = true;
+  }
+
 }
 
 void pulseWhite()
 {
-    dutyTime += cycleTime;      // update the duty cycle timer
-    pulseCycleTime += cycleTime;  // update the PWM cycle time
     // if the duty timer passes the PWM duty cycle time
     // turn the led OFF
     if(dutyTime > pwmDuty && ledON)  
@@ -53,10 +170,6 @@ void pulseWhite()
     {
       dutyTime = 0;
       digitalWrite(PIN_A0,HIGH);
-      // example of how to write debug info to the serial terminal
-      // char outString[30];
-      // sprintf(outString, "pwmTime = %d", cycleTime);
-      // Serial.println(outString);
       ledON = true;
     }
 
